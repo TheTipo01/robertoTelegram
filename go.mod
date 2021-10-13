859786567ec50fc54746bc764cadd933517cfacf
TheTipo01 <10187614+TheTipo01@users.noreply.github.com>
2021-10-13 21:33:36 +0200

    refactor: converted to libRoberto
    Also added codeql as a github action

--- go.mod
+++ go.mod
@@ -3,16 +3,12 @@ module robertoTelegram
 go 1.15
 
 require (
-	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/TheTipo01/libRoberto v0.0.0-20211013201512-9de6a407d94d
 	github.com/bwmarrin/lit v0.0.0-20190813132558-fd4b44871312
-	github.com/forPelevin/gomoji v0.0.0-20210718160015-5fcf0e405128
-	github.com/goccy/go-json v0.7.9
-	github.com/goodsign/monday v1.0.0
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/goccy/go-json v0.7.10 // indirect
+	github.com/kkyr/fig v0.3.0
 	github.com/spf13/viper v1.9.0
-	github.com/tidwall/gjson v1.9.1 // indirect
-	github.com/valyala/fasthttp v1.30.0 // indirect
-	golang.org/x/sys v0.0.0-20211001092434-39dca1131b70 // indirect
-	golang.org/x/text v0.3.7
+	github.com/tidwall/gjson v1.11.0 // indirect
+	golang.org/x/sys v0.0.0-20211105183446-c75c47738b0c // indirect
 	gopkg.in/tucnak/telebot.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -1,35 +1,30 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/base32"
+	"github.com/TheTipo01/libRoberto"
 	"github.com/bwmarrin/lit"
+	"github.com/kkyr/fig"
 	"github.com/spf13/viper"
 	tb "gopkg.in/tucnak/telebot.v2"
-	"io/ioutil"
 	"math/rand"
-	"net/http"
-	"os"
-	"os/exec"
 	"strings"
 	"time"
 )
+
+type config struct {
+	Token    string `fig:"token" validate:"required"`
+	Host     string `fig:"host" validate:"required"`
+	LogLevel string `fig:"loglevel" validate:"required"`
+	Database string `fig:"database" validate:"required"`
+}
+
+const audioExtension = ".mp3"
 
 var (
 	// Telegram token
 	token string
 	// HTTP server where we host .mp3
 	host string
-	// Array of adjectives
-	adjectives []string
-	// Gods
-	gods = []string{"Dio", "Gesù", "Madonna"}
-	// Emoji string replacer, replacing every emoji with it's description
-	emoji *strings.Replacer
-)
-
-const (
-	audioExtension = ".mp3"
 )
 
 func init() {
@@ -38,53 +33,31 @@ func init() {
 
 	lit.LogLevel = lit.LogError
 
-	viper.SetConfigName("config")
-	viper.SetConfigType("yml")
-	viper.AddConfigPath(".")
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found
-			lit.Error("Config file not found! See example_config.yml")
-			return
-		}
-	} else {
-		// Config file found
-		token = viper.GetString("token")
-		host = viper.GetString("host")
-
-		// Set lit.LogLevel to the given value
-		switch strings.ToLower(viper.GetString("loglevel")) {
-		case "logwarning", "warning":
-			lit.LogLevel = lit.LogWarning
-
-		case "loginformational", "informational":
-			lit.LogLevel = lit.LogInformational
-
-		case "logdebug", "debug":
-			lit.LogLevel = lit.LogDebug
-		}
-
-		initializeAdjectives()
-
-		// Create folders used by the bot
-		if _, err = os.Stat("./temp"); err != nil {
-			if err = os.Mkdir("./temp", 0755); err != nil {
-				lit.Error("Cannot create temp directory, %s", err)
-			}
-		}
-
-		emoji = emojiReplacer()
-
+	var cfg config
+	err := fig.Load(&cfg, fig.File("config.yml"))
+	if err != nil {
+		lit.Error(err.Error())
+		return
 	}
 
+	// Config file found
+	token = cfg.Token
+	host = cfg.Host
+
+	// Set lit.LogLevel to the given value
+	switch strings.ToLower(viper.GetString("loglevel")) {
+	case "logwarning", "warning":
+		lit.LogLevel = lit.LogWarning
+
+	case "loginformational", "informational":
+		lit.LogLevel = lit.LogInformational
+
+	case "logdebug", "debug":
+		lit.LogLevel = lit.LogDebug
+	}
 }
 
 func main() {
-	// Start HTTP server to serve generated .mp3 files
-	http.Handle("/temp/", http.StripPrefix("/temp", http.FileServer(http.Dir("./temp"))))
-	go http.ListenAndServe(":8069", nil)
-
 	// Create bot
 	b, err := tb.NewBot(tb.Settings{
 		Token:  token,
@@ -107,28 +80,28 @@ func main() {
 				results    = make(tb.Results, 1)
 			)
 
-			lit.Info("%s: %s", q.From.Username, q.Text)
+			lit.Debug("%s: %s", q.From.Username, q.Text)
 
 			// Various custom command
 			switch {
 			case strings.HasPrefix(upperQuery, "TRENO"):
-				query = strings.TrimSpace(searchAndGetTrain(strings.TrimPrefix(upperQuery, "TRENO ")))
+				query = strings.TrimSpace(libRoberto.SearchAndGetTrain(strings.TrimPrefix(upperQuery, "TRENO ")))
 				if query == "" {
 					query = "Nessun treno trovato, agagagaga!"
 				}
 
 			case strings.HasPrefix(upperQuery, "COVID"):
-				query = strings.TrimSpace(getCovid())
+				query = strings.TrimSpace(libRoberto.GetCovid())
 
 			case strings.HasPrefix(upperQuery, "BESTEMMIA"):
-				query = strings.TrimSpace(bestemmia())
+				query = strings.TrimSpace(libRoberto.Bestemmia())
 
 			default:
-				query = emojiToDescription(upperQuery)
+				query = libRoberto.EmojiToDescription(upperQuery)
 				isCommand = false
 			}
 
-			uuid = genAudio(query)
+			uuid = libRoberto.GenAudioMp3(query, time.Second*30)
 
 			// So the title of the result isn't all uppercase when there's no command
 			if !isCommand {
@@ -137,6 +110,7 @@ func main() {
 
 			// Create result
 			results[0] = &tb.VoiceResult{
+
 				URL:   host + uuid + audioExtension,
 				Title: query,
 			}
@@ -160,77 +134,4 @@ func main() {
 	lit.Info("robertoTelegram is now running")
 	b.Start()
 
-}
-
-// Generates audio from a string. Checks if it already exist before generating it
-func gen(text string, uuid string) {
-	_, err := os.Stat("./temp/" + uuid + audioExtension)
-
-	if err != nil {
-		var tts, ffmpeg *exec.Cmd
-		// Create a channel so that we can wait a maximum of 30 second before killing the processes
-		c := make(chan int)
-
-		go func() {
-			tts = exec.Command("balcon", "-i", "-o", "-enc", "utf8", "-n", "Roberto")
-			tts.Stdin = strings.NewReader(text)
-			ttsOut, _ := tts.StdoutPipe()
-			_ = tts.Start()
-
-			ffmpeg = exec.Command("ffmpeg", "-i", "pipe:0", "-f", "s16le", "-ar", "48000", "-ac", "2", "-f", "mp3", "./temp/"+uuid+audioExtension)
-			ffmpeg.Stdin = ttsOut
-			_ = ffmpeg.Run()
-
-			_ = tts.Wait()
-			c <- 1
-		}()
-
-		timer := time.NewTimer(time.Second * 30)
-		// If we get a response from the channel in a reasonable time, we just exit normally
-		select {
-		case <-c:
-			timer.Stop()
-			break
-		case <-timer.C:
-			// After 30 second, we can assume the process failed in some manner
-			if tts != nil && tts.Process != nil {
-				_ = tts.Process.Kill()
-			}
-
-			if ffmpeg != nil && ffmpeg.Process != nil {
-				_ = ffmpeg.Process.Kill()
-			}
-		}
-
-	}
-}
-
-// genAudio generates a mp3 file from a string, returning it's UUID (aka SHA1 hash of the text)
-func genAudio(text string) string {
-	h := sha1.New()
-	h.Write([]byte(text))
-	uuid := strings.ToUpper(base32.HexEncoding.EncodeToString(h.Sum(nil)))
-
-	gen(text, uuid)
-
-	return uuid
-}
-
-// Generates a bestemmia
-func bestemmia() string {
-	s1 := gods[rand.Intn(len(gods))]
-
-	s := s1 + " " + adjectives[rand.Intn(len(adjectives))]
-
-	if s1 == gods[2] {
-		s = s[:len(s)-2] + "a"
-	}
-
-	return s
-}
-
-// Reads adjectives
-func initializeAdjectives() {
-	foo, _ := ioutil.ReadFile("parole.txt")
-	adjectives = strings.Split(string(foo), "\n")
 }
